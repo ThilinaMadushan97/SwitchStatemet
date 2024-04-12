f2feeb990996586c201cd99aedc9cc3fb734a371
dell <you@example.com>
2024-04-12 10:56:52 +0530

    Add project files.

---
+++ SwitchStatemet/Program.cs
@@ -0,0 +1,28 @@
+﻿DisplayMesurement(2.0);
+DisplayMesurement(-2.0);
+DisplayMesurement(double.NaN);
+DisplayMesurement(5.0);
+DisplayMesurement(6.0);
+
+
+void DisplayMesurement(double value)
+{
+    switch (value)
+    {
+        case < 0.0:
+            Console.WriteLine($"Your value is low {value}");
+            break;
+
+        case > 1.0:
+            Console.WriteLine($"Your value is high {value}");
+            break;
+
+        case double.NaN:
+            Console.WriteLine($"Fail mesurement");
+            break;
+
+        default:
+            Console.WriteLine($"Your mesurment is {value}");
+            break;
+    }
+}
